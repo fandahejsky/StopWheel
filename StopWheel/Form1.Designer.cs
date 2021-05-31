8ee9742e1e4098084b342e3b616b2391bde8c23a
fandahejsky <73832271+fandahejsky@users.noreply.github.com>
2021-05-31 14:37:29 +0200

    Initial commit

---
+++ StopWheel/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿
+namespace StopWheel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(37, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 600);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 661);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ StopWheel/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StopWheel
+{
+    public partial class Form1 : Form
+    {
+
+        int sirkaP;
+        int vyskaP;
+        int velikost = 300;
+        int velikost2 = 330;
+        int poziceX = 135;
+        int poziceY = 300;
+
+        double rotace;
+        double radiany;
+
+        int delkaZony = 120;
+        int castNaKruhu;
+
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            sirkaP = panel1.Width;
+            vyskaP = panel1.Height;
+
+            rotace = 90;
+            radiany = rotace * (Math.PI / 180);
+
+            Graphics g = panel1.CreateGraphics();
+            g.DrawEllipse(Pens.Black, (sirkaP/2)-(velikost/2), (vyskaP / 2) - (velikost / 2), velikost, velikost);
+            g.DrawEllipse(Pens.Black, (sirkaP/2)-(velikost2/2), (vyskaP / 2) - (velikost2 / 2), velikost2, velikost2);
+
+            nakresliCaru(g);
+            nahodnaPoziceZony(g);
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (poziceX >= 135 && poziceX <= 300 && poziceY <= 300 && poziceY >= 135)
+            {
+                poziceX= (int)(Math.Cos(radiany) * ((double)poziceX - (double)velikost2) - (double)Math.Sin(radiany) * ((double)poziceY - velikost2) + (double)velikost2);
+                
+            }
+           
+
+            panel1.Refresh();
+        }
+
+        private void nakresliCaru(Graphics g)
+        {
+
+            g.DrawLine(Pens.Blue, sirkaP/2, vyskaP/2, poziceX, poziceY);
+        }
+
+        private void nahodnaPoziceZony(Graphics g)
+        {
+            g.DrawArc(Pens.Red, 150, 150, 300, 300, castNaKruhu, delkaZony);
+
+            g.DrawArc(Pens.Red, 135, 135, 330, 330, castNaKruhu, delkaZony);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            castNaKruhu = rnd.Next(0, 361);
+        }
+    }
+}
